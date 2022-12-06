34905cd66e1441d15341029a87b530f2372ccabd
nico <ainililia@163.com>
2022-12-06 18:40:47 +0800

    main

--- main.go
+++ main.go
@@ -17,17 +17,19 @@ func main() {
 	parser.Parse(parser.NewNLex([]byte(str)))
 
 	for _, fn := range parser.Ast.Functions {
-		fmt.Println(fn.Name)
+		fmt.Print("func ", fn.Name, "(")
 		for _, arg := range fn.Args {
-			fmt.Println(arg.Name, arg.Type)
+			fmt.Print(arg.Name, arg.Type)
 		}
+		fmt.Println("){")
 		for _, block := range fn.Blocks {
 
 			switch block.Type {
 			case ast.AssignmentBlock:
 				ass := block.Value.(ast.Assignment)
-				fmt.Printf("%v = %v, %v\n", ass.Variable, ass.Value.Value, ass.Value.Type)
+				fmt.Printf("    %v = %v\n", ass.Variable, ass.Value.Value)
 			}
 		}
+		fmt.Println("}")
 	}
 }
